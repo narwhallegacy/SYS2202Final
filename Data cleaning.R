28e1be12b3c939a1f96039221183f2b80e9a91c3
narwhallegacy <glcorbin4@gmail.com>
2020-04-07 11:23:28 -0400

    Setup
    Uploaded .csv files and began data cleaning script

---
+++ Data cleaning.R
@@ -0,0 +1,22 @@
+library("TTR")
+library("smoother")
+library("ggplot2")
+library("gcookbook")
+library("tidyverse")
+library("cluster")
+library("MASS")
+
+#-------Read in data-------------------------------------------------
+
+arrests = read.csv("Arrests.csv", header=TRUE)
+Football = read.csv("College_Football_2000_2018.csv", header=TRUE)
+dogtags = read.csv("Dog_Tags.csv", header=TRUE)
+greenTransport = read.csv("Green_Infrastructure_Transportation.csv", header=TRUE)
+RoadCenterlines = read.csv("Road_Centerlines.csv", header=TRUE)
+transit2017 = read.csv("transit_2017.csv", header=TRUE)
+transit2018 = read.csv("transit_2018.csv", header=TRUE)
+trannsit2019 = read.csv("transit_2019.csv", header=TRUE)
+transit2020 = read.csv("transit_2020.csv", header=TRUE)
+unemploymentYearly = read.csv("Unemployment.csv", header=TRUE)
+unemploymentMonthly = read.csv("Unemployment_monthly.csv", header=TRUE)
+uvaBasketball = read.csv("UVA2019_2020BasketballSeason.csv", header=TRUE)
