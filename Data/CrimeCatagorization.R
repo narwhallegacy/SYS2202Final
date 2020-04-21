eaff467f109ade8bbcc7f23c7682f91236ad1e40
narwhallegacy <glcorbin4@gmail.com>
2020-04-21 12:26:33 -0400

    Worked on creating broad catagories for crime types

---
+++ Data/CrimeCatagorization.R
@@ -0,0 +1,61 @@
+library("tidyverse")
+
+crime = read.csv("Sports_Crime.csv", header = TRUE)
+
+Theft = ((crime$crime_type == 'Larceny - Shoplifitng') | (crime$crime_type == 'Burglary') | 
+  (crime$crime_type == 'Larceny - All Other') | (crime$crime_type == 'Larceny - From Motor Vehicle') | (crime$crime_type == 'Larceny - Of Veh Parts/Access') | 
+  (crime$crime_type == 'Larceny - Pocket Picking') | (crime$crime_type == 'Larceny - Theft from Building') | (crime$crime_type == 'Motor Vehicle Theft'))
+
+crimeCat = factor(Theft, labels = c('', 'Theft'))
+
+VehAccid = ((crime$crime_type == 'Accident - w/out Injuries') | (crime$crime_type == 'Hit and Run') | 
+                      (crime$crime_type == 'Accident Property Damage') | (crime$crime_type == 'Accident w/Injuries'))
+
+crimeCat = paste(crimeCat, factor(VehAccid, labels = c('', 'Vehicle Accident')), sep = "")
+
+Animal = ((crime$crime_type == 'Animal - Bite') | (crime$crime_type == 'Animal Complaint'))
+
+crimeCat = paste(crimeCat, factor(Animal, labels = c('', 'Animal-related')), sep = "")
+
+Noncrim = ((crime$crime_type == 'Assist Agency - Backup/Assist') | (crime$crime_type == 'Assist Citizen - Medical') | 
+             (crime$crime_type == 'Assist Citizen - Welfare Check') | (crime$crime_type == 'Assist Citizen - Medical') | (crime$crime_type == 'Found/Recovered Property') | 
+             (crime$crime_type == 'Lost/FoundProperty') | (crime$crime_type == 'Missing Person-adult') | (crime$crime_type == 'Missing Person-juvenile') | 
+             (crime$crime_type == 'Comm Relations Initiative - CRI') | (crime$crime_type == 'Misc - Non-Criminal Call'))
+
+crimeCat = paste(crimeCat, factor(Noncrim, labels = c('', 'Non-criminal report')), sep = "")
+
+DisCon = ((crime$crime_type == 'Disorderly Conduct') | (crime$crime_type == 'Disturbance - Non Domestic') | 
+            (crime$crime_type == 'Domestic Disturbance') | (crime$crime_type == 'Shots Fired/Illegal Hunting') | (crime$crime_type == 'Suspicious Activity') | 
+            (crime$crime_type == 'Suspicious Person') | (crime$crime_type == 'Trespass') | (crime$crime_type == 'Vandalism'))
+
+crimeCat = paste(crimeCat, factor(DisCon, labels = c('', 'Disorderly Conduct')), sep = "")
+
+SubAbuse = ((crime$crime_type == 'Driving Under the Influence') | (crime$crime_type == 'Drug Investigation') | 
+              (crime$crime_type == 'Drug/Narcotics Violation') | (crime$crime_type == 'Drunkeness DIP') | (crime$crime_type == 'Liquor Law Violation') | 
+              (crime$crime_type == 'Narcotics'))
+
+crimeCat = paste(crimeCat, factor(SubAbuse, labels = c('', 'Substance Abuse')), sep = "")
+
+Fraud = ((crime$crime_type == 'Embezzelment') | (crime$crime_type == 'Forgery/Counterfeiting') | 
+           (crime$crime_type == 'Fraud-credit card') | (crime$crime_type == 'Fraud-false pretense') | (crime$crime_type == 'Fraud-impersonation') | 
+           (crime$crime_type == 'Fraud-wire fraud'))
+
+crimeCat = paste(crimeCat, factor(Fraud, labels = c('', 'Fraud')), sep = "")
+
+Threat = ((crime$crime_type == 'Extortion/Blackmail') | (crime$crime_type == 'Harassment') | 
+            (crime$crime_type == 'Phone Calls - Threat or Obscene'))
+
+crimeCat = paste(crimeCat, factor(Threat, labels = c('', 'Threats')), sep = "")
+
+sexOff = ((crime$crime_type == 'Sex Offense') | (crime$crime_type == 'Sex Offense - Forcible Rape') | 
+            (crime$crime_type == 'Sex Offense - Forcible Sodomy') | (crime$crime_type == 'Sex Offense-forcible fondling'))
+
+crimeCat = paste(crimeCat, factor(sexOff, labels = c('', 'Sex Offense')), sep = "")
+
+other = ((crime$crime_type == 'Death Investigation - DOA') | (crime$crime_type == 'Runaway') | 
+           (crime$crime_type == 'Solicitation illegal w/o permit') | (crime$crime_type == 'Towed Vehicle') | (crime$crime_type == 'Traffic Stops') | 
+           (crime$crime_type == 'Unauthorized Use of Motor Veh') | (crime$crime_type == 'Warrant Service') | (crime$crime_type == 'Weapons Violations'))
+
+crimeCat = paste(crimeCat, factor(other, labels = c('', 'Other')), sep = "")
+
+crimeCat
